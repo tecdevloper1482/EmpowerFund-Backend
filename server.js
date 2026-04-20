205c2c01f0da6b342e62f3a178f5e0d094290ce2
Simran <kaursimranjeet97084@gmail.com>
2026-04-20 13:29:14 +0530

    finally fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Frontend URL
+  origin:   process.env.FRONTEND_URL, // Frontend URL
   credentials: true
 }));
 app.use(express.json());
